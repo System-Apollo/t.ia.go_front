2c89ac97dabc2ad941d790e0c14990947713a1fb
MF-Digital-Law <advprojetos@meirelesefreitas.com.br>
2024-10-13 17:57:38 -0300

    feat: atualixado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
-import Home from './Components/Home/Home';
-import IA from './Components/IA/ia'; // Certifique-se de que o caminho está correto
+import Home from './Home/Home';
+import IA from './IA/ia'; // Certifique-se de que o caminho está correto
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Tela inicial */}
-        <Route path="/ia" element={<IA />} />
-      </Routes>
-    </Router>
+    <ChakraProvider >
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Tela inicial */}
+          <Route path="/ia" element={<IA />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
