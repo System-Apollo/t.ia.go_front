cec8114df63ce4284bc8486752a71bd38f453ab0
leticia nobre <leticiavidal2910@gmail.com>
2024-11-04 08:59:48 -0300

    ✨ feat: added new feature

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../styles/Login.css';
+import Logo from '../Assets/TiagoLogo.png'
 
 const Login = () => {
     return (
         <div className="login-container">
             <div className='login-box'>
-                <h2>Fazer Login</h2>
-                <form>
-                    <label>Email:</label>
-                    <input className="input" type="email" placeholder="Digite seu email" />
-                    <label>Senha:</label>
-                    <input className="input" type="password" placeholder="Digite sua senha" />
+                <img src={Logo} alt="Ícone de login" className="login-icon"/>
+                <div className="text-box">
+                    <h2 className="login-title">Fazer Login</h2>
+                    <p className='login-text'>Entre com seu email para falar com Tiago</p>
+                </div>
+                <form className='form'>
+                    {/* <label>Email:</label> */}
+                    <input className="input" type="email" placeholder="Email" />
+                    {/* <label>Senha:</label> */}
+                    <input className="input" type="password" placeholder="Senha" />
                     <button className="button" type="submit">Entrar</button>
                 </form>
             </div>
