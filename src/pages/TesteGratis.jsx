a3913f7e33766bf757f98539af39eba8fc9e28e7
PedroItaloQ <peedroitalo2906@gmail.com>
2024-11-04 16:13:02 -0300

    refat: updated in TesteGratis

--- src/pages/TesteGratis.jsx
+++ src/pages/TesteGratis.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/TesteGratis.css';
 import Logo from '../Assets/TiagoLogo.png'
+import { useNavigate } from 'react-router-dom';
+import { apiLogin } from '../utils/PostLogin';
 
 const TesteGratis = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const credentials = {
+            username: username,
+            password: password,
+        }
+
+        try{
+            const res = await apiLogin.Login(credentials);
+            console.log('Login bem-sucedido', res.data);
+            navigate('/ia');
+        } catch(error) {
+            setError('Error ao fazer login. Verifique suas credenciais.');
+            console.log(error);
+        }
+    }
+
     return (
         <div className="trial-container">
             <div className="teste-gratis-box">
@@ -11,13 +36,14 @@ const TesteGratis = () => {
                     <h2 className="login-title">Teste grátis com email</h2>
                     <p className='login-text'>Comece sua experiência com Tiago</p>
                 </div>
-                <form className='form'>
+                <form className='form' onSubmit={handleSubmit}>
                     {/* <label>Email:</label> */}
                     <input className="input" type="email" placeholder="Email" />
                     {/* <label>Senha:</label> */}
                     <input className="input" type="password" placeholder="Senha" />
                     <button className="button" type="submit">Iniciar Teste</button>
                 </form>
+                {error && <p className='error-message'>{error}</p>}
             </div>
         </div>
     );
