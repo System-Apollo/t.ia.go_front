70175c5a5d303e1490c8536e25295668943c92de
leticia nobre <leticiavidal2910@gmail.com>
2024-11-04 08:54:23 -0300

    refat: refat app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../src/styles/App.css'
 import Home from './pages/Home';
+import Login from './pages/Login';
+import TesteGratis from './pages/TesteGratis';
 import IA from './pages/ia'; // Certifique-se de que o caminho está correto
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} /> {/* Tela inicial */}
           <Route path="/ia" element={<IA />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/teste-gratis" element={<TesteGratis />} />
         </Routes>
       </Router>
     </ChakraProvider>
