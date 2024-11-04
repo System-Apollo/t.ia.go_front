558ff768f9d6dda94bde8eba1adf103d10360701
leticia nobre <leticiavidal2910@gmail.com>
2024-11-04 08:58:35 -0300

    ✨ feat: added new feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { TypeAnimation } from 'react-type-animation'
-import '../styles/Home.css'; // Importar o arquivo CSS
+import { TypeAnimation } from 'react-type-animation';
+import '../styles/Home.css';
 
-import Logo2 from '../Assets/ti.png'
-import Logo1 from '../Assets/logo.png'
-import Logo3 from '../Assets/go1.png'
-
-
-
-
-
+import Logo2 from '../Assets/ti.png';
+import Logo1 from '../Assets/logo.png';
 
 const Home = () => {
-    const navigate = useNavigate(); // Hook para redirecionamento
+    const navigate = useNavigate();
 
-    const handleAcessar = () => {
-        // Redireciona para a página da IA ao clicar no botão
-        navigate('/ia');
+    const handleLogin = () => {
+        navigate('/login');
     };
 
-
+    const handleFreeTrial = () => {
+        navigate('/teste-gratis');
+    };
 
     return (
         <div className="home-container">
-
             <img src={Logo1} className="images1" alt="Logo1" height={30} width={180} />
             <img src={Logo2} className="images" alt="Logo2" />
             <div>
@@ -40,27 +34,16 @@ const Home = () => {
                             'Resoluções Assertivas',
                             1000,
                         ]}
-                        wrapper='span'
+                        wrapper="span"
                         speed={50}
                         repeat={Infinity}
-
                     />
                 </h1>
             </div>
-            {/* <h1 className="title-text">Insights!</h1> */}
-
-            <div onClick={handleAcessar} className="uiverse">
-                {/* <span class="tooltip">Chamar o TIAGO</span> */}
-                <span>
-                Iniciar Análise
-                </span>
-                {/* <span class="top-key"></span>
-                <span class="text">Iniciar Análise</span>
-                <span class="bottom-key-1"></span>
-                <span class="bottom-key-2"></span> */}
-                {/* Fale com o TIA <img src={Logo3} className="icon" alt="Logo2" /> */}
+            <div className="button-container">
+                <button onClick={handleLogin} className="login-button">Fazer Login</button>
+                <button onClick={handleFreeTrial} className="trial-button">Teste Grátis</button>
             </div>
-
         </div>
     );
 };
